4420a7aa3a0e5a3881da9b58f42031ffd4f20c80
thisoban <thisoban_1995@hotmail.com>
2023-10-24 14:35:19 +0200

    Add project files.

---
+++ MMContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace mixandmatchv2
+{
+    public class MMContext : DbContext
+    {
+        static readonly string connectionString = "Server=localhost; Port=3308;User ID=root;Password=password123;Database=mm";
+        public DbSet<Job> Jobs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+    public class Job
+    {
+        public Guid jobid { get; set; }
+        public string jobname { get; set; }
+        public string jobdescription { get; set; }
+        public string jobtype { get; set; }
+
+    }
+}
