712f4b130c694ce78a484baa30c5a4298f65b688
thisoban <thisoban_1995@hotmail.com>
2023-10-25 14:29:11 +0200

    issue fixed with api calls and dependency injection with context

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using mixandmatchv2;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddScoped<MMContext>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
